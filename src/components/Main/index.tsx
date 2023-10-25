a3c62af15c4892ae62453038fe5581cfb548a29a
lenonfelippi <lenonfelippi1@gmail.com>
2023-10-24 22:31:34 -0300

    add styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,10 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
+    <S.Wrapper>
       <h1>Testando o jest</h1>
-    </main>
+    </S.Wrapper>
   )
 }
 
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,10 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: green;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+`
